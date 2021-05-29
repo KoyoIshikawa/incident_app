d36ee38dfba86131647c889c31ee7162cebabbcb
KoyoIshikawa <koyoi561@gmail.com>
2021-05-29 22:52:30 +0900

    csvインポート処理を分けた

--- lib/import_csv.rb
+++ lib/import_csv.rb
@@ -3,8 +3,21 @@ require "csv"
 class ImportCsv
   # CSVデータのパスを引数として受け取り、インポート処理を実行
   def self.import(path)
-     CSV.foreach(path, headers: true) do |row|
-       OsName.create!(row.to_h)
-     end
+    # インポートするための空配列
+    list = []
+    # CSVファイルからインポートしたデータを格納
+      CSV.foreach(path, headers: true) do |row|
+        list << row.to_h
+      end
+    # メソッドの戻り値をインポートしたデータの配列とする
+    list
+  end
+  def self.user_data
+    # importクラスメソッドを呼び出し，ユーザーデータの配列を生成
+    list = import('db/csv_data/OsName_data.csv')
+
+    puts "インポート処理を開始"
+    OsName.create!(list)
+    puts "インポート完了!"
   end
 end
