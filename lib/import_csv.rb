aa2d120ca7f4567c7b40d09f2e2d884678da1b4a
KoyoIshikawa <koyoi561@gmail.com>
2021-05-29 22:34:27 +0900

    カラム名の取得を簡易化

--- lib/import_csv.rb
+++ lib/import_csv.rb
@@ -4,9 +4,7 @@ class ImportCsv
   # CSVデータのパスを引数として受け取り、インポート処理を実行
   def self.import(path)
      CSV.foreach(path, headers: true) do |row|
-       OsName.create!(
-         name: row["name"],
-       )
+       OsName.create!(row.to_h)
      end
   end
 end
