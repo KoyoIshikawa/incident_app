a412a27b94f8e28cb2f7bdff48c9ed4795162580
KoyoIshikawa <koyoi561@gmail.com>
2021-06-06 14:00:41 +0900

    ストロングパラーメーターとnew,createメソッドを定義

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,15 +1,19 @@
 class IncidentsController < ApplicationController
   before_action :authenticate_user!, except: :index
   def index
+    
   end
 
   def show
   end
 
   def new
+    @incident = Incident.new
   end
 
   def create
+    incident = Incident.create!(incident_params)
+    redirect_to incident
   end
 
   def edit
@@ -19,5 +23,10 @@ class IncidentsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def incident_params
+    params.require(:incident).permit(:incident, :solution)
   end
 end
