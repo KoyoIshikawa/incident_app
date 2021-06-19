333c9cd1864d3295ebb6e8e0ce8d57b51134e4c2
KoyoIshikawa <koyoi561@gmail.com>
2021-06-19 16:28:45 +0900

    文字修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many : articles, dependent: :destroy
-  has_many :articled_incidents, through: :articles. sourse: :incident
+  has_many :articled_incidents, through: :articles, sourse: :incident
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
