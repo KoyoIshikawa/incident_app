3503e1632b45e07c5afa110791ab7f8312453682
KoyoIshikawa <koyoi561@gmail.com>
2021-06-19 16:53:24 +0900

    sourseをsourceに修正

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,6 +1,6 @@
 class Incident < ApplicationRecord
-  has_many : articles, dependent: :destroy
-  has_many : articled_users, through: :articles, source: :user
+  has_many :articles, dependent: :destroy
+  has_many :articled_users, through: :articles, source: :user
   validates :incident, presence: true
   validates :solution, presence: true
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many : articles, dependent: :destroy
-  has_many :articled_incidents, through: :articles, sourse: :incident
+  has_many :articles, dependent: :destroy
+  has_many :articled_incidents, through: :articles, source: :incident
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
