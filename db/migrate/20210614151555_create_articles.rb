b36ef8771d4be410efa9dd2c47f05d14bdcc259f
KoyoIshikawa <koyoi561@gmail.com>
2021-06-19 14:42:14 +0900

    記事に空白禁止制約の導入

--- db/migrate/20210614151555_create_articles.rb
+++ db/migrate/20210614151555_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration[6.1]
     create_table :articles do |t|
       t.references :user, null: false, foreign_key: true
       t.references :incident, null: false, foreign_key: true
-      t.text :content
+      t.text :content, null: false
 
       t.timestamps
     end
