bf15fcdf12144854d6b1b10196a809e5852bbcc2
KoyoIshikawa <koyoi561@gmail.com>
2021-06-19 17:23:19 +0900

    インシデントテーブルとユーザーテーブルの紐付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :incidents, dependent: :destroy
   has_many :articles, dependent: :destroy
   has_many :articled_incidents, through: :articles, source: :incident
   # Include default devise modules. Others available are:
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_151555) do
+ActiveRecord::Schema.define(version: 2021_06_19_081227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2021_06_14_151555) do
     t.text "solution"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_incidents_on_user_id"
   end
 
   create_table "os_names", force: :cascade do |t|
@@ -64,4 +66,5 @@ ActiveRecord::Schema.define(version: 2021_06_14_151555) do
 
   add_foreign_key "articles", "incidents"
   add_foreign_key "articles", "users"
+  add_foreign_key "incidents", "users"
 end
