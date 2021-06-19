ca92572f03993945315fbb9cb1d94729ea3d0b25
KoyoIshikawa <koyoi561@gmail.com>
2021-06-19 14:44:16 +0900

    インシデントに空白禁止制約の導入

--- db/migrate/20210527120426_create_incidents.rb
+++ db/migrate/20210527120426_create_incidents.rb
@@ -1,8 +1,8 @@
 class CreateIncidents < ActiveRecord::Migration[6.1]
   def change
     create_table :incidents do |t|
-      t.text :incident
-      t.text :solution
+      t.text :incident, null: false
+      t.text :solution, null: false
 
       t.timestamps
     end
