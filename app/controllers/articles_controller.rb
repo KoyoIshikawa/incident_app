5872dfc254251d2eb7aff2d7b57a8823bc7e111e
KoyoIshikawa <koyoi561@gmail.com>
2021-06-24 00:54:26 +0900

    不要部分を削除

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,4 @@
 class ArticlesController < ApplicationController
-  def new
-    @incident = Incident.find(params[:id])
-    @article = Article.new
-  end
   def create
     article = current_user.articles.create!(content: article_params[:content], incident_id: params[:incident_id])
     redirect_to "/incidents/#{article.incident.id}",  notice: "作成しました"
