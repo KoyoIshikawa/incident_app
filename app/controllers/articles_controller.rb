33605675ba651fe29be6ef237ccd59f6e0c2c326
KoyoIshikawa <koyoi561@gmail.com>
2021-06-26 01:36:14 +0900

    記事更新機能を追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,23 @@
 class ArticlesController < ApplicationController
+  # before_action :set_article, only: %i[edit update destroy]
+
   def create
     article = current_user.articles.create!(content: article_params[:content], incident_id: params[:incident_id])
     redirect_to "/incidents/#{article.incident.id}",  notice: "作成しました"
   end
 
   def edit 
-    @incident = Incident.find(params[:id])
-    @article = Article.find(params[:id])
+    @incident = Incident.find(params[:incident_id])
+    @article = @incident.articles.find(params[:id])
   end
 
   def update
-
+    incident = Incident.find(params[:incident_id])
+    article = incident.articles.find(params[:id])
+    article.update!(article_params)
+    redirect_to incident, notice: "更新しました"
   end
-  
+
   def destroy
   end
 
@@ -20,4 +25,9 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:content)
   end
+
+  # def set_article
+  #   @article = current_user.articles.find_by(id: params[:id], incident_id: params[:incident_id])
+  #   redirect_to root_path, alert: "権限がありません" if @article.nil?
+  # end
 end
