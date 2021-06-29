718c1c80534bd95bb5aeb3229bdbfa044201132f
KoyoIshikawa <koyoi561@gmail.com>
2021-06-29 10:45:58 +0900

    incidentsテーブルに外部キーを追加

---
+++ db/migrate/20210629014407_add_details_to_incidents.rb
@@ -0,0 +1,7 @@
+class AddDetailsToIncidents < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :incidents, :os_name, null: false, foreign_key: true
+    add_reference :incidents, :status, null: false, foreign_key: true
+    add_reference :incidents, :coding_lang, null: false, foreign_key: true
+  end
+end
