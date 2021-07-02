5ceef96e48aab9dd35e2c498c8c48859b875f238
KoyoIshikawa <koyoi561@gmail.com>
2021-07-02 13:18:02 +0900

    検索結果を標示する画面を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root "incidents#index"
   resources :incidents do 
+    collection do
+      get 'search'
+    end
     resources :articles, only: [:create, :destroy, :edit, :update]
   end 
 end
