664531755f3ee7324c00539c1ca5fd3d64778e69
KoyoIshikawa <koyoi561@gmail.com>
2021-07-21 17:29:20 +0900

    メールアドレス、パスワードを環境変数で記載

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,8 +86,8 @@ Rails.application.configure do
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => 'smtp.gmail.com',
-    :user_name => "koyoi561@gmail.com",
-    :password => "unhpusokoeemjqve",
+    :user_name => ENV["GOOGLE_MAIL_ADDRESS"],
+    :password => ENV["GOOGLE_MAILER_PASSWORD"],
     :authentication => 'login'
   }
 end
