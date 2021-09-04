8b7f3d3488ce5cebcbecc025da1d9cbe3db0ec54
KoyoIshikawa <koyoi561@gmail.com>
2021-09-04 13:42:26 +0900

    seedファイルにユーザ名を登録できよう実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, uniqueness: true, presence: true
   def self.guest
     find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
       user.username = "ゲスト"
     end
   end
-  validates :username, uniqueness: true, presence: true
 end
