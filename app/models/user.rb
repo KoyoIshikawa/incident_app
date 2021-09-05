4ed3e9a0bd83d5b3aef47cf54a598c503710b36f
KoyoIshikawa <koyoi561@gmail.com>
2021-09-06 05:22:58 +0900

    デバイスの認証にのみユーザ名を利用する設定を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ApplicationRecord
   has_many :articled_incidents, through: :articles, source: :incident
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :authentication_keys => [:username], :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, uniqueness: true, presence: true
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
   def self.guest
     find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
