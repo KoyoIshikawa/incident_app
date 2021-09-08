7450cbdbf1cf9ee07f9028c3369ff2034afbca87
KoyoIshikawa <koyoi561@gmail.com>
2021-09-08 10:30:31 +0900

    アカウント編集画面の戻るボタンを大きくした

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :articled_incidents, through: :articles, source: :incident
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :authentication_keys => [:username], :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:username]
   validates :username, uniqueness: true
   validates :email, uniqueness: true
   def self.guest
