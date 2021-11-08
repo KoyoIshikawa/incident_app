233466c987b59c3e8448e0f412d5b0bacac04b0f
KoyoIshikawa <koyoi561@gmail.com>
2021-11-09 08:36:01 +0900

    production.rb の一番上に，EC2サーバーにログインするユーザー名、サーバーのロールを記述

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,11 @@
+server "54.64.229.69", user: "koyoIshikawa", roles: %w{app db web}
+
+set :ssh_options, {
+  keys: %w(~/.ssh/aws_incident_app.pem),
+  forward_agent: true,
+  auth_methods: %w(publickey),
+}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
