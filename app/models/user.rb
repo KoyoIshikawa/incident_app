c7123020da694bfe93fcc38a9349d07fc7ee49c1
KoyoIshikawa <koyoi561@gmail.com>
2021-11-21 12:30:07 +0900

    emailに＠を含めるバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
   has_many :incidents, dependent: :destroy
   has_many :articles, dependent: :destroy
   has_many :articled_incidents, through: :articles, source: :incident
@@ -7,7 +9,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
          :authentication_keys => [:username]
   validates :username, uniqueness: true
-  validates :email, uniqueness: true
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX }
+
   def self.guest
     find_or_create_by!(email: "guest@example.com") do |user|
       user.password = SecureRandom.urlsafe_base64
