93fe6bddd663d6753196f1b7f97469a749338dcb
KoyoIshikawa <koyoi561@gmail.com>
2021-11-28 23:30:33 +0900

    ユーザ名に空白禁止設定追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
          :authentication_keys => [:username]
-  validates :username, uniqueness: true
+  validates :username, uniqueness: true, presence: true
   validates :email,
             presence: true,
             uniqueness: true,
