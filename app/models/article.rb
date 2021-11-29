ed36937adda3646ca35aade240600dc9a83536ac
KoyoIshikawa <koyoi561@gmail.com>
2021-11-29 10:19:57 +0900

    incidentテーブルのincident,solutionおよびarticleテーブルのcontentに文字数制限を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
   belongs_to :incident
-  validates :content, presence: true
+  validates :content,
+            presence: true,
+            length: { maximum: 2000 }
 end
--- app/models/incident.rb
+++ app/models/incident.rb
@@ -5,7 +5,12 @@ class Incident < ApplicationRecord
   belongs_to :coding_lang
   has_many :articles, dependent: :destroy
   has_many :articled_users, through: :articles, source: :user
-  validates :incident, presence: true
-  validates :solution, presence: true
+  validates :incident, 
+            presence: true,
+            length: { maximum: 20000 }
+  validates :solution, 
+            presence: true,
+            length: { maximum: 200 }
+
   
 end
