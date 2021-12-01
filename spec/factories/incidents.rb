fae75503b7f4ed8721bb4459da161eb5d4e6241c
KoyoIshikawa <koyoi561@gmail.com>
2021-12-01 19:26:15 +0900

    spec/factories/incidents.rbのcoding_lang_idを指定からcoding_langを指定するよう変更

--- spec/factories/incidents.rb
+++ spec/factories/incidents.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     ImportCsv.os_name_data
     ImportCsv.status_data
     ImportCsv.coding_lang_data
-    coding_lang_id { CodingLang.find(rand(CodingLang.first.id..CodingLang.last.id)).id }
+    coding_lang { CodingLang.find(rand(CodingLang.first.id..CodingLang.last.id)) }
     incident { Faker::Lorem.word }
     os_name { OsName.find(rand(OsName.first.id..OsName.last.id)) }
     solution { Faker::Lorem.paragraph }
