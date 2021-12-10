f1122688d64ca07dd99b5c8930906b9615627452
KoyoIshikawa <koyoi561@gmail.com>
2021-12-10 15:13:37 +0900

    indexのリクエストスペックをリファクタリング

--- spec/requests/incidents_request_spec.rb
+++ spec/requests/incidents_request_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Incidents", type: :request do
   describe "GET #index" do
-    it "リクエストが成功する" do
-      create_list(:incident, 3)
-      get(incidents_path)      
-      expect(response).to have_http_status(:ok)
-    end
-    it "incident が表示されている" do
-      incident1 = create(:incident)
-      incident2 = create(:incident)
-      incident3 = create(:incident)
-      get(incidents_path)
-      expect(response.body).to include(incident1.incident, incident2.incident, incident3.incident)
+    subject { get(incidents_path) }
+    context "インシデントが存在するとき" do 
+      before { create_list(:incident, 3) }
+      it "リクエストが成功する" do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "incident が表示されている" do
+        subject
+        expect(response.body).to include(*Incident.pluck(:incident))
+      end
     end
   end
 end
