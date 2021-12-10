7bad388488f380fa450077db76695847ba0a8226
KoyoIshikawa <koyoi561@gmail.com>
2021-12-10 14:55:08 +0900

    incidentモデルのincidentが表示されることを確認するテストを実装

--- spec/requests/incidents_request_spec.rb
+++ spec/requests/incidents_request_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe "Incidents", type: :request do
       get(incidents_path)      
       expect(response).to have_http_status(:ok)
     end
+    it "incident が表示されている" do
+      incident1 = create(:incident)
+      incident2 = create(:incident)
+      incident3 = create(:incident)
+      get(incidents_path)
+      expect(response.body).to include(incident1.incident, incident2.incident, incident3.incident)
+    end
   end
 end
