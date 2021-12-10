3ec27a948db937631c30ceedd7472d3b97f852ff
KoyoIshikawa <koyoi561@gmail.com>
2021-12-10 15:22:38 +0900

    showのリクエストスペックの条件を記載

--- spec/requests/incidents_request_spec.rb
+++ spec/requests/incidents_request_spec.rb
@@ -16,4 +16,35 @@ RSpec.describe "Incidents", type: :request do
       end
     end
   end
+  describe "GET #show" do
+    subject { get(incident_path) }
+    context "インシデントが存在するとき" do 
+      before { create_list(:incident, 3) }
+      it "リクエストが成功する" do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "incidentが表示されている" do
+        subject
+        expect(response.body).to include(*Incident.pluck(:incident))
+      end
+
+      it "solutionが表示される" do
+
+      end
+      it "status"do
+
+      end
+      it "update_atが表示される"do
+      
+      end
+      it "os_nameが表示される"do
+      
+      end
+      it "create_atが表示される"do
+      
+      end
+    end
+  end
 end
