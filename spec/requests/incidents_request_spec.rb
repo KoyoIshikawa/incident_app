9d982760a29400933588f6db83eacbb430a2e37d
KoyoIshikawa <koyoi561@gmail.com>
2021-12-29 19:42:10 +0900

    リクエストスペックの説明を記載

--- spec/requests/incidents_request_spec.rb
+++ spec/requests/incidents_request_spec.rb
@@ -17,33 +17,195 @@ RSpec.describe "Incidents", type: :request do
     end
   end
   describe "GET #show" do
-    subject { get(incident_path) }
     context "インシデントが存在するとき" do 
-      before { create_list(:incident, 3) }
       it "リクエストが成功する" do
-        subject
-        expect(response).to have_http_status(:ok)
       end
 
-      it "incidentが表示されている" do
-        subject
-        expect(response.body).to include(*Incident.pluck(:incident))
+      it "インシデントが表示されている" do
       end
 
-      it "solutionが表示される" do
+      it "解決方法がが表示される" do
 
       end
-      it "status"do
+      it "ユーザが表示される" do 
 
       end
-      it "update_atが表示される"do
+
+      it "作成日時が表示される"do
       
       end
-      it "os_nameが表示される"do
+
+      it "更新日時が表示される"do
       
       end
-      it "create_atが表示される"do
+
+      it "ステータスが表示される"do
+
+      end
+
+      it "OSが表示される"do
       
+      end
+
+      it "言語が表示される" do
+        
+      end
+
+      context "インシデントに付随する記事が存在するとき" do
+        it "記事一覧が表示される" do 
+
+        end
+      end
+      context "インシデントに付随する記事が存在しないとき" do
+        it "記事一覧が表示される" do 
+          
+        end 
+      end
+      
+    end
+    context ":idに対応するインシデントが存在しないとき" do
+      it "エラーが発生する" do
+        
+      end
+    end
+  end
+  describe "GET #new" do
+    it "リクエストが成功する" do
+      
+    end
+  end 
+  describe "POST #create" do
+    context "パラメータが正常なとき" do
+      it "インシデントが保存される" do
+        
+      end
+      it "詳細ページにリダイレクトされる" do
+        
+      end
+    end
+    context "パラメータが異常なとき" do
+      it "リクエストが成功する" do
+        
+      end
+      it "インシデントが保存されない" do
+        
+      end
+      it "新規投稿ページにレンダリングされる" do 
+
+      end
+    end
+  end
+  describe "GET #edit" do
+    context "インシデントが存在するとき" do 
+      it "リクエストが成功する" do
+      end
+
+      it "インシデントが表示されている" do
+      end
+
+      it "解決方法がが表示される" do
+
+      end
+      it "ユーザが表示される" do 
+
+      end
+
+      it "作成日時が表示される"do
+      
+      end
+
+      it "更新日時が表示される"do
+      
+      end
+
+      it "ステータスが表示される"do
+
+      end
+
+      it "OSが表示される"do
+      
+      end
+
+      it "言語が表示される" do
+        
+      end
+
+      context "インシデントに付随する記事が存在するとき" do
+        it "記事一覧が表示される" do 
+
+        end
+      end
+      context "インシデントに付随する記事が存在しないとき" do
+        it "記事一覧が表示される" do 
+          
+        end 
+      end
+      
+    end
+    context ":idに対応するインシデントが存在しないとき" do
+      it "エラーが発生する" do
+        
+      end
+    end
+  end
+  describe "PATCH #update" do
+    context "パラメータが正常なとき" do
+      it "リクエストが成功する" do
+      end
+
+      it "インシデントが更新される" do
+
+      end
+
+      it "解決方法が更新される" do
+
+      end
+      it "更新日時が更新される"do
+      
+      end
+
+      it "ステータスが更新される"do
+
+      end
+
+      it "OSが更新される"do
+      
+      end
+
+      it "言語が更新される" do
+        
+      end
+      it "詳細ページにリダイレクトされる" do
+        
+      end
+    end
+    context "インシデントのパラメータが異常なとき" do
+      it "リクエストが成功する" do
+        
+      end
+      it "インシデントが更新されない" do
+        
+      end
+      it "解決方法が更新されない" do
+
+      end
+      it "更新日時が更新されない"do
+      
+      end
+
+      it "ステータスが更新されない"do
+
+      end
+
+      it "OSが更新されない"do
+      
+      end
+
+      it "言語が更新されない" do
+        
+      end
+      it "編集ページにレンダリングされる" do
+        
       end
     end
   end
